3ea4dc40639c6380a33fe1044f1c8f116e32ef11
kjg <kjg9190@hotmail.com>
2021-10-31 01:18:00 +0200

    correct update period

--- shellscripts/uptime.sh
+++ shellscripts/uptime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # . /home/$USER/envmonitor/venv/bin/activate
 . ~/envmonitor/venv/bin/activate
-python ~/envmonitor/uptime.py --period 2 # default is every 30 minutes
+python ~/envmonitor/uptime.py --period 1800 # default is every 30 minutes
 
